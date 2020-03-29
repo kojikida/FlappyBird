cf466f5b9820c21e15d6ee36f4586b97e7cba000
kojikida <koji.in.kiribati@gmail.com>
2020-03-29 15:00:45 +0900

    オリジナル版（カリキュラム ）

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //SKVに型を変換する
+        let skView = self.view as! SKView
+        
+        //FPSをほ表示する
+        skView.showsFPS = true
+        
+        //ノードの数を表示する
+        skView.showsNodeCount = true
+        
+        //ビューと同じサイズでシーンを作成する
+        //SKSceneからGameSceneに変更する
+        let scene = GameScene(size:skView.frame.size)
+        
+        //ビューにシーンを表示する
+        skView.presentScene(scene)
+        
     }
 
 
